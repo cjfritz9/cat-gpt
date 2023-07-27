d39913662bf0a4a72b9249c59d7665a0ef5b239d
CJ <cjfritz9@gmail.com>
2023-07-27 15:52:57 -0600

    solved existing database function issue that was causing new issues

--- server/src/db/users.ts
+++ server/src/db/users.ts
@@ -76,9 +76,7 @@ export const getUserByEmail = async (email: string) => {
   try {
     const users: any = db.collection('users');
     const snapshot: any = await users.where('email', '==', email).get();
-    console.log('snapshot get user by email', snapshot);
-    console.log('snapshot empty?: ', snapshot.empty);
-    if (!snapshot) return 'Error: No user found';
+    if (snapshot.empty) return 'Error: No user found';
     let user: any = {};
     snapshot.forEach((doc: any) => {
       Object.assign(user, { id: doc.id });
